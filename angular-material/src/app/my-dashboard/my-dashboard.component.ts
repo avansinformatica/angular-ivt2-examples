dce408474627ea673559e2c36f03cbc4f545317c
Robin Schellius <r.schellius@avans.nl>
2018-10-08 10:25:27 +0200

    Updated.

--- angular-material/src/app/my-dashboard/my-dashboard.component.ts
+++ angular-material/src/app/my-dashboard/my-dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ArrayStack } from '../../model/stack'
 
 @Component({
   selector: 'my-dashboard',
@@ -13,48 +12,4 @@ export class MyDashboardComponent {
     { title: 'Card 3', cols: 1, rows: 2 },
     { title: 'Card 4', cols: 1, rows: 1 }
   ];
-
-  inputstrings: string[] = [
-    '(({}))[{}{}]',
-    '(({}))(([]))',
-    '()'
-  ];
-
-  stack: ArrayStack<any> = new ArrayStack<any>();
-
-  checkBalanced = (input: string) => {
-    for(let i = 0; i < input.length; i++){
-      const char = input.charAt(i);
-      // console.log(char);
-      switch(char) {
-        case '(':
-        case '[]]': 
-        case '{': 
-          console.log('push ' + char);
-          this.stack.push(char);
-          break;
-        case ')':
-        case ']':
-        case '}':
-          console.log('pop ' + char);
-          this.stack.pop();
-          break;
-      }      
-    }
-    console.log('${text} = ' + (this.stack.isEmpty() ? 'Balanced' : 'Niet OK'), input);
-  }
-
-
-  public check(){
-    this.inputstrings.forEach(element => {
-      try { 
-        this.checkBalanced(element);
-      }
-      catch(error){
-        console.log(error.message);
-      }
-    });
-  }
-
-
 }
