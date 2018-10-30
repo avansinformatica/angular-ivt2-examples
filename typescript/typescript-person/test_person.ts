80147e8c1924a3afb73a9f476b7a82e6582cbe57
Dion Koeze <dj.koeze@avans.nl>
2018-10-30 15:39:07 +0100

    Added school example

---
+++ typescript/typescript-person/test_person.ts
@@ -0,0 +1,20 @@
+import {Person} from './person';
+
+// create two people
+let jane = new Person();
+let john = new Person();
+
+// change properties of one
+john.firstName = 'John';
+john.lastName = 'Smith';
+john.birthDate = new Date(1960,2,23);
+
+// check the output of default values for jane
+console.log(jane);
+console.log(jane.getName());
+console.log(jane.getAge());
+
+// check the changed properties of john
+console.log(john);
+console.log(john.getName());
+console.log(john.getAge());
