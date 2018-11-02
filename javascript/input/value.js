d65e3b45bec29d9bc8ff89de83b6859160911155
Robin Schellius <r.schellius@avans.nl>
2018-11-02 16:26:14 +0100

    Updated.

--- javascript/input/value.js
+++
@@ -1,8 +0,0 @@
-function process_value() {
-    let input = document.getElementById('myinput').value;
-    let result = document.getElementById('result');
-
-    console.log('received ' + input);
-
-    result.innerText = input;
-}
