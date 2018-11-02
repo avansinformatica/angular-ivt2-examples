7c07e2768b45a5fffc3addceba289b09f78e7f7d
Robin Schellius <r.schellius@avans.nl>
2018-11-02 16:25:46 +0100

    Updated

--- angular/angular-modules/src/app/modules/users/user.service.ts
+++ angular/angular-modules/src/app/modules/users/user.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { Http, RequestOptions, Headers } from '@angular/http';
-import { User, UserRole } from './user.model';
+import { Http } from '@angular/http';
+import { User } from './user.model';
 import { EntityService } from '../../core/base/entity.service';
 import { EntitySerializer } from '../../core/base/entity.model';
 
 @Injectable()
 export class UsersService extends EntityService<User> {
 
+  /**
+   * In this example we fetch the data from a file. 
+   * Later this will be the URL to the 
+   */
   readonly apiUrl = '../../assets/dummy-courses.json'; 
 
   constructor(http: Http) {
