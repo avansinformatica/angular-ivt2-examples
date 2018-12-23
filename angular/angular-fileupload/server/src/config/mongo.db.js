1b743aaec1c52db36ef469c9edcf58f6ac785710
Robin Schellius <r.schellius@avans.nl>
2018-12-23 15:28:01 +0100

    Updated Docker config

--- angular/angular-fileupload/server/src/config/mongo.db.js
+++ angular/angular-fileupload/server/src/config/mongo.db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { logger, dbHost, dbPort, dbUser, dbDatabase, dbPassword }  = require('./config');
 
-// Gebruik es6 promises ipv mongoose mpromise
+// Use es6 promises instead of mongoose mpromise
 mongoose.Promise = global.Promise;
 
 const dbUrl = process.env.NODE_ENV === 'production' ?
@@ -15,30 +15,22 @@ const options = {
     useCreateIndex: true,
     useFindAndModify: false,
     autoIndex: false
-    // autoReconnect: true,                // Doesn't seem to work...
-    // numberOfRetries : Number.MAX_VALUE, // Never stop trying to reconnect
-    // reconnectInterval: 2000,
-    // poolSize: 10,                       // Maintain up to 10 socket connections
-    // // If not connected, return errors immediately rather than waiting for reconnect
-    // // bufferMaxEntries: 0,
-    // connectTimeoutMS: 30000,            // Give up initial connection after milliseconds
-    // socketTimeoutMS: 45000,             // Close sockets after 45 seconds of inactivity
-    // family: 4                           // Use IPv4, skip trying IPv6
 }
 
 mongoose.connect(dbUrl, options)
 const connection = mongoose.connection
-    .once('open', function () { logger.trace('MongoDB connection opened') })
-    .on('connecting', function () { logger.info(`Connecting to ${dbUrl}`) })
-    .on('connected', function () { logger.warn('MongoDB connected!') })
-    .on('reconnected', function () { logger.info('MongoDB reconnected') })
+    .once('open', () => { logger.info('MongoDB connection opened') })
+    .on('connecting', () => { logger.info(`Connecting to ${dbUrl}`) })
+    .on('connected', () => { logger.info('MongoDB connected!') })
+    .on('reconnected', () => { logger.info('MongoDB reconnected') })
     .on('error', function (error) { 
         logger.error(error.toString())
         mongoose.disconnect()
     })
-    .on('disconnected', function () { 
+    .on('disconnected', () => { 
         logger.error('MongoDB disconnected!')
         setTimeout(() => {
+            // Try to reconnect every 5 secs. 
             mongoose.connect(dbUrl, options )
         }, 5000);
     })
